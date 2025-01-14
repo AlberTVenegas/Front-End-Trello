d724cac8ccdfb203d35a19538b1b29609cad0e63
Albert Venegas <albertvenegas24@gmail.com>
2025-01-14 15:22:39 -0300

    fix end point

--- src/api/trello.js
+++ src/api/trello.js
@@ -4,28 +4,28 @@ import axios from 'axios';
 
 
 export const getallTasks = () => {
-    return axios.get('https://back-end-trello.onrender.com/trello/api/v1/tareas/')
+    return axios.get('https://back-end-trello-production.up.railway.app/trello/api/v1/tareas/')
 }
 
 export const updateStateTask = (id, stateUpdate) => {
-    return axios.patch(`https://back-end-trello.onrender.com/trello/api/v1/tareas/${id}/`, { status: stateUpdate })
+    return axios.patch(`https://back-end-trello-production.up.railway.app/trello/api/v1/tareas/${id}/`, { status: stateUpdate })
 }
 
 export const addTask = (task) => {
-    return axios.post('https://back-end-trello.onrender.com/trello/api/v1/tareas/', task)
+    return axios.post('https://back-end-trello-production.up.railway.app/trello/api/v1/tareas/', task)
 }
 
 export const getAllProyect = () => {
-    return axios.get('https://back-end-trello.onrender.com/trello/api/v1/tableros/')
+    return axios.get('https://back-end-trello-production.up.railway.app/trello/api/v1/tableros/')
 
 }
 export const addTablero = (tablero) => {
-    return axios.post('https://back-end-trello.onrender.com/trello/api/v1/tableros/', tablero)
+    return axios.post('https://back-end-trello-production.up.railway.app/trello/api/v1/tableros/', tablero)
 }
 export const deleteTask = (id) => {
-    return axios.delete(`https://back-end-trello.onrender.com/trello/api/v1/tareas/${id}/`)
+    return axios.delete(`https://back-end-trello-production.up.railway.app/trello/api/v1/tareas/${id}/`)
 }
 
 export const deleteTablero = (id) => {
-    return axios.delete(`https://back-end-trello.onrender.com/trello/api/v1/tableros/${id}/`)
+    return axios.delete(`https://back-end-trello-production.up.railway.app/trello/api/v1/tableros/${id}/`)
 }
